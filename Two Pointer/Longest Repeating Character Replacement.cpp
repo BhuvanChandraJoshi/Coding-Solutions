d2d19e09ef645e0c5d273b7f619536ff106b53ed
BhuvanChandraJoshi <76677898+BhuvanChandraJoshi@users.noreply.github.com>
2022-11-01 11:45:49 +0530

    Update Longest Repeating Character Replacement.cpp

--- Two Pointer/Longest Repeating Character Replacement.cpp
+++ Two Pointer/Longest Repeating Character Replacement.cpp
@@ -1,26 +1,17 @@
 //Link: https://leetcode.com/problems/longest-repeating-character-replacement/
 
 int characterReplacement(string s, int k) {
-        vector<int> counts(26, 0);
-        int start = 0;
-        int maxCharCount = 0;
-        int n = s.length();
-        int result = 0;
-        for(int end = 0; end < n; end++){
-            counts[s[end]-'A']++;
-            if(maxCharCount < counts[s[end]-'A']){
-                maxCharCount = counts[s[end]-'A'];
+        int size = s.size(); int ret = 0;
+        vector<int> count(26, 0);
+        int start = 0; int end = 0; int globalMaxFreq = 0; 
+        for(; end<size; end++){
+            count[s[end]-'A'] += 1;
+            globalMaxFreq = max(globalMaxFreq, count[s[end]-'A']);
+            if((end-start+1)-globalMaxFreq > k) {
+                ret = max(ret, (end-start));
+                count[s[start]-'A'] -= 1;
+                start += 1;
             }
-            while(end-start-maxCharCount+1 > k){
-                counts[s[start]-'A']--;
-                start++;
-                for(int i = 0; i < 26; i++){
-                    if(maxCharCount < counts[i]){
-                        maxCharCount = counts[i];
-                    }
-                }
-            }
-            result = max(result, end-start+1);
         }
-        return result;
+        return max(ret, end-start);
     }
