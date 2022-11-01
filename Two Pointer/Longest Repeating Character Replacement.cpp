4157daf1e2b9517c6a549b31e304b839640b24ea
BhuvanChandraJoshi <76677898+BhuvanChandraJoshi@users.noreply.github.com>
2022-11-01 22:02:22 +0530

    Update Longest Repeating Character Replacement.cpp

--- Two Pointer/Longest Repeating Character Replacement.cpp
+++ Two Pointer/Longest Repeating Character Replacement.cpp
@@ -1,5 +1,6 @@
 //Link: https://leetcode.com/problems/longest-repeating-character-replacement/
 
+//Implementation A
 int characterReplacement(string s, int k) {
     int res = 0, maxf = 0;
     unordered_map<int, int> count;
@@ -11,4 +12,21 @@ int characterReplacement(string s, int k) {
             count[s[i - res]]--;
     }
     return res;
+}
+
+//Implmentation B                       (both the approacges are same only implementation is different)
+ int characterReplacement(string s, int k) {
+    int size = s.size(); int ret = 0;
+    vector<int> count(26, 0);
+    int start = 0; int end = 0; int globalMaxFreq = 0; 
+    for(; end<size; end++){
+        count[s[end]-'A'] += 1;
+        globalMaxFreq = max(globalMaxFreq, count[s[end]-'A']);
+        if((end-start+1)-globalMaxFreq > k) {
+            ret = max(ret, (end-start));
+            count[s[start]-'A'] -= 1;
+            start += 1;
+        }
+    }
+    return max(ret, end-start);
 }
